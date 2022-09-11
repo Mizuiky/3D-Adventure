c77d70fa4f833cacd8c48d5080bd630cf8909d96
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2022-09-11 12:27:37 -0300

    Add player animations

---
+++ 3D Adventure Game/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : AnimationBase
+{
+    public void OnVictory() { }
+
+    public void OnLose() { }
+}
